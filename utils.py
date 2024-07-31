2ff143e8cd7e4e2a19f460f642122cf313172edc
strakajk <jakubstraka123@gmail.com>
2024-07-31 19:51:59 +0200

    Add tools for work with index files

--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
 import numpy as np
+import os
+import pandas as pd
+from tqdm import tqdm
 
 
 def crop_pad_image(image: np.ndarray, bbox: np.ndarray, border: float = 0.25, color: int = 114) -> (np.ndarray, list):
@@ -50,3 +53,42 @@ def crop_pad_image(image: np.ndarray, bbox: np.ndarray, border: float = 0.25, co
     cropped_image = image[y0:y1, x0:x1]
 
     return cropped_image, new_bbox
+
+
+def get_state_counts(index_folder: str):
+    """
+    Load index files from a given folder and count the occurrences of each state value.
+
+    Parameters:
+        index_folder (str): The folder containing the index files.
+    """
+    index_files = [os.path.join(index_folder, file) for file in os.listdir(index_folder) if ".csv" in file]
+    index_files = [pd.read_csv(file, dtype={"file_names": str, "state": float}) for file in tqdm(index_files)]
+
+    index_file = pd.concat(index_files)
+    print(len(index_file))
+    counts = dict(index_file["state"].value_counts())
+    for state, count in counts.items():
+        print(f"State {state}:   {count}")
+
+
+def reset_state(index_file: str, old_state: int, new_state: int):
+    """
+    Reset the state of all records in an index file to a new state.
+
+    Parameters:
+        index_file (str): The path to the index file or a folder with index files.
+        old_state (int): The old state value to be replaced.
+        new_state (int): The new state value to replace the old state with.
+    """
+    if index_file.endswith(".csv"):
+        index_files = [index_file]
+    else:
+        index_files = [os.path.join(index_file, file) for file in os.listdir(index_file) if ".csv" in file]
+
+    for file in tqdm(index_files):
+        index_file = pd.read_csv(file, dtype={"file_names": str, "state": float})
+        index_file.loc[index_file["state"] == old_state, 'state'] = new_state
+        index_file.to_csv(file, index=False)
+
+
