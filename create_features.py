291c9b3311f03b21556cf22decec19e70632add9
strakajk <jakubstraka123@gmail.com>
2024-08-09 12:51:57 +0200

    Merge keypoint feature scripts

--- create_features.py
+++ create_features.py
@@ -6,7 +6,7 @@ import os
 import pandas as pd
 import argparse
 
-from data.h2s import How2SignDataset
+from data.h2s import How2SignDatasetJSON
 
 
 def save_to_h5(features_list_h5, label, index_dataset, chunk_batch, chunk_size):
@@ -36,7 +36,7 @@ def add_to_h5(clip_name, clip_features, index_dataset, chunk_batch, chunk_size):
         feature = clip_features[index_dataset:index_dataset + last_chunk_size]
         index_dataset, chunk_batch = save_to_h5(features_list_h5, feature, index_dataset, chunk_batch,
                                                 last_chunk_size)
-        
+
 def get_keypoints(json_data):
     right_hand_landmarks = []
     left_hand_landmarks = []
@@ -70,12 +70,15 @@ def get_json_files(json_dir):
     json_files = [os.path.join(json_dir, json_file) for json_file in os.listdir(json_dir) if json_file.endswith('.json')]
     return json_files
 
-        
+
 def get_args_parser():
     parser = argparse.ArgumentParser('', add_help=False)
 
-    parser.add_argument('--dataset_split', type=str)
-    parser.add_argument('--root_folder', type=str)
+    parser.add_argument('--input_folder', type=str)
+    parser.add_argument('--output_folder', type=str)
+    parser.add_argument('--dataset_name', type=str)
+    parser.add_argument('--split_name', default="train", type=str)
+
     parser.add_argument('--annotation_file', type=str)
 
     return parser
@@ -83,95 +86,56 @@ def get_args_parser():
 
 if __name__ == "__main__":
     args = get_args_parser().parse_args()
-    
-    # prepare variables
-    dataset_split = args.dataset_split
-    root_folder = args.root_folder
 
-    dataset_file = os.path.join(root_folder, "h5_dataset", f"H2S_{dataset_split}.h5")
+    # prepare names
+    output_file_name = f"{args.dataset_name}.keypoints.{args.split_name}.0.h5"
+    output_features_name = f"{args.dataset_name}.keypoints.{args.split_name}.json"
+    os.makedirs(args.output_folder, exist_ok=True)
 
-    split = 0
-    output_folder =  os.path.join(root_folder, "h5_features")
-    output_file_name = f"H2S.keypoints.{dataset_split}.{split}.h5"
-    output_features_name = f"H2S.keypoints.{dataset_split}.json"
+    # prepare mapping between clip names and video names
+    clip_to_video = {}
+    clip_names = os.listdir(args.input_folder)
+    clip_names = [file for file in clip_names if file.endswith(".mp4")]
+    for idx in range(len(clip_names)):
+        name_split = clip_names[idx].split(".")[:-1]
+        clip_names[idx] = ".".join(name_split)
+    if args.annotation_file:
+        annotations = pd.read_csv(args.annotation_file, sep='\t')
+        _clip_to_video = dict(zip(annotations.SENTENCE_NAME, annotations.VIDEO_ID))
+        for name in clip_names:
+            clip_to_video[name] = _clip_to_video[name]
+    else:
+        for name in clip_names:
+            name_split = name.split(".")[:-1]
+            video_name = ".".join(name_split)
+            clip_to_video[name] = video_name
 
-    annotation_file = args.annotation_file
-    
-    
-    json_dir = os.path.join(root_folder, dataset_split, "clips_cropped")
-
-    # create h5 dataset
-    json_list = get_json_files(json_dir)
-    with h5py.File(dataset_file, 'w') as f:
-        for json_file in tqdm(json_list):
-
-            with open(json_file, 'r') as file:
-                keypoints_meta = json.load(file)
-
-            video_name = os.path.basename(json_file).replace(".json", "")
-            video_name_g = f.create_group(video_name)
-
-            metadata = video_name_g.create_group('metadata')
-            metadata.create_dataset(name='start_time', data=0, dtype=np.float32)
-            metadata.create_dataset(name='end_time', data=0, dtype=np.float32)
-            metadata.create_dataset(name='video_name', data=video_name)
-            metadata.create_dataset(name='full_video_name', data="")
-
-            video_name_g.create_dataset(name='sentence', data="")
-            joints_g = video_name_g.create_group('joints')
-
-            pose_landmarks, right_hand_landmarks, left_hand_landmarks, face_landmarks = get_keypoints(keypoints_meta)
-
-            joints_g.create_dataset(name='pose_landmarks', data=pose_landmarks, dtype=np.float32)
-            joints_g.create_dataset(name='right_hand_landmarks', data=right_hand_landmarks, dtype=np.float32)
-            joints_g.create_dataset(name='left_hand_landmarks', data=left_hand_landmarks, dtype=np.float32)
-            joints_g.create_dataset(name='face_landmarks', data=face_landmarks, dtype=np.float32)
-    
-    
-    
     # load dataset
-    dataset = How2SignDataset(
-        dataset_file, 
+    dataset = How2SignDatasetJSON(
+        args.input_folder,
+        clip_to_video,
         kp_normalization=[
             "global-pose_landmarks",
             "local-right_hand_landmarks",
             "local-left_hand_landmarks",
             "local-face_landmarks",
             ]
-    )
-
-    annotations = pd.read_csv(annotation_file, sep='\t')
-    video_names = set(annotations.VIDEO_ID)
-
-    video_to_idx = {}
-    for idx, data in enumerate(tqdm(dataset)):
-        clip_name = data["video_name"]
-
-        for video_name in video_names:
-            if clip_name.startswith(video_name):
-                break
-
-        if video_name in video_to_idx:
-            video_to_idx[video_name].append(idx)
-        else:
-            video_to_idx[video_name] = [idx]
-            
+    )    
+    print(len(dataset))
             
     # save features
-    f_out = h5py.File(os.path.join(output_folder, output_file_name), 'w')
-    dt = h5py.vlen_dtype(np.dtype('float16'))
+    f_out = h5py.File(os.path.join(args.output_folder, output_file_name), 'w')
 
     metadata = {}
-    for video_name in tqdm(video_to_idx):
-
-        idxs = video_to_idx[video_name]
+    for data in tqdm(dataset):
+        video_name = data[0]["video_name"]
         video_h5 = f_out.create_group(video_name)
-        for idx in idxs:
-            data = dataset[idx]
-            features = data["data"]
-            clip_name = data["video_name"]
+        
+        for clip in data:
+            features = clip["data"]
+            clip_name = clip["clip_name"]
 
-            metadata[video_name] = split
+            metadata[video_name] = 0
 
             # save features in hd5
             add_to_h5(
@@ -184,7 +148,7 @@ if __name__ == "__main__":
 
     f_out.close()
 
-    with open(os.path.join(output_folder, output_features_name), "w") as f:
+    with open(os.path.join(args.output_folder, output_features_name), "w") as f:
         json.dump(metadata, f)
-    
+
 
